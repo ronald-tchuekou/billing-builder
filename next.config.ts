ff638802a6ea3bb888467fd26821b749a43b3692
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2026-05-18 16:39:17 +0100

    Update allowed dev origin for billing-builder in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['localhost:3000', '7138-2605-59c0-1ef2-2810-dde0-6196-c058-2417.ngrok-free.app', 'billing-builder-q8hjtcupf-ronald-tchuekous-projects.vercel.app'],
+  allowedDevOrigins: ['localhost:3000', '7138-2605-59c0-1ef2-2810-dde0-6196-c058-2417.ngrok-free.app', 'https://billing-builder.vercel.app'],
   experimental: {
     serverActions: {bodySizeLimit: "5mb"},
   },
